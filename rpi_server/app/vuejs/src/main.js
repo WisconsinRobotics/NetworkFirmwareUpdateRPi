0f5cb3eba90208872eea0bce3e908b8ba50a20dc
jeritgeorge <george6@wisc.edu>
2018-11-16 18:23:07 -0600

    Change to standard js

--- rpi_server/app/vuejs/src/main.js
+++ rpi_server/app/vuejs/src/main.js
@@ -1,26 +1,26 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
 
-import MaterialKit from "./plugins/material-kit";
+import MaterialKit from './plugins/material-kit'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-Vue.use(MaterialKit);
+Vue.use(MaterialKit)
 
 const NavbarStore = {
   showNavbar: false
-};
+}
 
 Vue.mixin({
-  data() {
+  data () {
     return {
       NavbarStore
-    };
+    }
   }
-});
+})
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
