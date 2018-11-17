a22162df017005a5281f8be677b32e874f7f3b03
nardecchia <nardecchia@wisc.edu>
2018-11-16 19:42:28 -0600

    Working Front End Test as of now.

---
+++ IntegrationTests/README.txt
@@ -0,0 +1,17 @@
+An automated image-based verification test covering the entire front-end. Guarantees that any changes to out code won’t affect the functionality of our web application. The test outputs feedback for each individual unit, and if a unit fails, the test will indicate what failed. It tests the process start to finish, from setting up the server to validating that an image is transferred to the appropriate location.
+
+==Prerequisites==
+Requires that "npm install" has been run at some point in the past. 
+
+==Installing==
+Download and Install Eclipse : https://www.eclipse.org/downloads/
+Download and Install SikuliX : https://launchpad.net/sikuli/sikulix
+Add the Sikuli API to Eclipse.
+
+==Running test==
+Simply add the test.java file to a project and include the imgs folder in the project folder. Ensure that no instances of Google Chrome are open before clicking Run.
+
+==Author: Wisconsin Robotic Image Programmer Dev Team==
+
+
+
--- IntegrationTests/src/TestMainWebApp.java
+++ IntegrationTests/src/TestMainWebApp.java
@@ -17,6 +17,7 @@ public class TestMainWebApp{
             		try {
             		s.wait(2.0);
             		s.type(Key.WIN);
+            		s.wait(2.0);
             		s.type("git bash");
             		s.type(Key.ENTER);
             		s.wait(3.0);
@@ -24,11 +25,15 @@ public class TestMainWebApp{
             		s.type(Key.ENTER);
             		s.type("cd ECE506");
             		s.type(Key.ENTER);
-            		s.type("cd BootstrapGitFolder");
+            		s.type("cd WhatHaveIDone");
             		s.type(Key.ENTER);
             		s.type("cd NetworkFirmwareUpdateRPi");
             		s.type(Key.ENTER);
-            		s.type("cd vue-material-kit-v1.0.0");
+            		s.type("cd rpi_server");
+            		s.type(Key.ENTER);
+            		s.type("cd app");
+            		s.type(Key.ENTER);
+            		s.type("cd vuejs");
             		s.type(Key.ENTER);
             		s.type("npm run dev");
             		s.type(Key.ENTER);
@@ -99,12 +104,23 @@ public class TestMainWebApp{
             			System.out.println("Failed while choosing a file to upload");
             		}
             		
+            //validate history shows images and that images can be uploaded
+            		try {
+            			s.click("imgs/historyTab.jpg");
+            			s.wait(1.0);
+            			
+            			System.out.println("Successfully flashed image from history!");
+            		}
+            		catch(Exception e) {
+            			System.out.println("Error loading image from history");
+            		}
+            		
             //validate tab choosing works
             		try {
             			s.wait(1.0);
-            			s.click("imgs/historyTab.jpg");
-            			s.wait(1.0);
             			s.click("imgs/githubTab.jpg");
+            			s.wait(1.0);
+            			s.click("imgs/historyTab.jpg");
             			s.wait(1.0);
             			s.click("imgs/uploadTab.jpg");
             			s.wait(2.0);
@@ -116,8 +132,10 @@ public class TestMainWebApp{
             		
             //Validate Wisconsin Robotics link works
             		try {
-            			r.keyPress(KeyEvent.VK_DOWN);
-            			r.keyRelease(KeyEvent.VK_DOWN);
+            			for(int i = 0; i < 8; i++) {
+                			r.keyPress(KeyEvent.VK_DOWN);
+                			r.keyRelease(KeyEvent.VK_DOWN);
+                			};
             			s.rightClick("imgs/roboticsLink.jpg");
             			s.click("imgs/newTab.png");
             			s.wait(2.0);
@@ -172,13 +190,14 @@ public class TestMainWebApp{
             		catch(Exception e) {
             			System.out.println("Error loading github page");
             		}
-            		
+            	//duplicate this above and include delete statements to validate history and github tabs	
             //Validate image file landed in correct location
             		try {
             		r.keyPress(KeyEvent.VK_WINDOWS);
             		r.keyPress(KeyEvent.VK_E);
             		r.keyRelease(KeyEvent.VK_E);
             		r.keyRelease(KeyEvent.VK_WINDOWS);
+            		s.wait(1.0);
             		//add - validate file
             		s.click("imgs/closeFolder.jpg");
             		System.out.println("Image successfully sent to correct folder");
@@ -186,6 +205,28 @@ public class TestMainWebApp{
             		catch(Exception e) {
             			System.out.println("Error - Image not sent to folder!");
             		}
+            		//close browser
+            		s.wait(2.0);
+            		r.keyPress(KeyEvent.VK_CONTROL);
+            		r.keyPress(KeyEvent.VK_W);
+            		r.keyRelease(KeyEvent.VK_W);
+            		r.keyRelease(KeyEvent.VK_CONTROL);
+            		System.out.println("Chrome Closed");
+            		//close bash
+            		s.wait(2.0);
+            		try{
+            			s.click("imgs/bash.jpg");
+            		}
+            		catch(Exception e) {
+            			
+            		}
+            		r.keyPress(KeyEvent.VK_CONTROL);
+            		r.keyPress(KeyEvent.VK_C);
+            		r.keyRelease(KeyEvent.VK_C);
+            		r.keyRelease(KeyEvent.VK_CONTROL);
+            		s.click("imgs/closeFolder.jpg");
+            		System.out.println("Git Bash Closed");
+            		Sikulix.popup("TESTING COMPLETED!!!!!!!");
             }
             catch(Exception e) {
             	System.out.println("Undefined Fatal Error");
