6a66c888eaff3f58fc8cbcb9cb3349dbf4bec497
jeritgeorge <george6@wisc.edu>
2018-11-25 01:42:46 -0600

    Fix navbar scroll transparency with smaller header

--- rpi_server/app/vuejs/src/router.js
+++ rpi_server/app/vuejs/src/router.js
@@ -13,7 +13,7 @@ export default new Router({
       name: 'index',
       components: { default: Index, header: MainNavbar, footer: MainFooter },
       props: {
-        header: { colorOnScroll: 400 },
+        header: { colorOnScroll: 100 },
         footer: { backgroundColor: 'black' }
       }
     }
