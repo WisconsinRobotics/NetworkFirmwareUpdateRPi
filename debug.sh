11f816a0b627761b1926d7af46df549f23b4c7a1
Matt Szekely <mjszekely@wisc.edu>
2018-11-26 15:23:15 -0600

    Combined npm.sh and run.sh into debug.sh. Type "sh debug.sh" to run both servers

--- debug.sh
+++ debug.sh
@@ -1,3 +1,10 @@
+cd ./rpi_server/app/vuejs
+if [ ! -d "node_modules" ]; then
+    npm install
+fi
+npm run dev &
+
+cd ../../..
 # Set the path variable for the server script
 export FLASK_APP='rpi_server/app/routes.py'
 
@@ -6,3 +13,4 @@ export FLASK_ENV='development'
 
 # Run the server locally
 python3 -m flask run
+
