6d11183b5916ec5c0268230dd95d6bec9a440650
jeritgeorge <george6@wisc.edu>
2018-12-06 17:53:27 -0600

    Start adding tests

--- rpi_server/app/vue-test-utils-jest-example-master/test/NavPills.spec.js
+++ rpi_server/app/vue-test-utils-jest-example-master/test/NavPills.spec.js
@@ -1,9 +1,28 @@
 import { mount } from '@vue/test-utils'
 import NavPills from '@/components/NavPillsSection.vue'
+import Vue from 'vue'
+import jsdom from 'jsdom'
 
+const renderer = require('vue-server-renderer').createRenderer()
 describe('NavPillsSection.vue', () => {
   it('Checks its a vue instance', () => {
     const wrapper = mount(NavPills)
     expect(wrapper.isVueInstance()).toBeTruthy()
+  }), 
+  it('Checks History images are displayed ', () => {
+    const ClonedComponent = Vue.extend(NavPills)
+    const wrapper = new ClonedComponent({
+      data() {
+        return {
+          history: ['image1', 'image2'],
+        }
+      }
+    }).$mount()
+    renderer.renderToString(wrapper, (err, str) => {
+      // const dom = new jsdom.JSDOM(str)
+      // wrapper.find('md-list-item-button').trigger('click')
+      // const image = dom.window.document.getElementById('image')
+      expect(str).toContain('image1')
+    }) 
   })
 })
